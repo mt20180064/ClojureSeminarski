62a3a341f2bb3bcf4eaf7a4466baf1ec7ff165ba
martodmih <martodmih@gmail.com>
2023-12-28 12:54:47 +0100

    implementirane metode za unos podataka o igracima i cuvanje istih u globalnu varijablu vektorskog tipa

--- escape-game/src/escape_game/core.clj
+++ escape-game/src/escape_game/core.clj
@@ -28,37 +28,74 @@
                1-da
                2-ne")
  (if (= "1" (get-input))
-   (println (+ score 1))
-   (println (+ score 2)))
+   (println (inc score))
+   (println (inc score) (inc score)))
  (println "U redu. Da li je soba linearna?
                1-da
                2-ne")
- (if (= "1" (get-input))
-   (println (+ score 1))
-    (println (+ score 2)))
+(if (= "1" (get-input))
+  (println (inc score))
+  (println (inc score) (inc score)))
  (println "Da li je fokus sobe na zagonetkama (1) ili mehanizmima (2)?")
- (if (= "1" (get-input))
-   (println (+ score 1))
-    (println (+ score 2)) )(println score)))
-  (defn -main
-    [& args] 
-     (println "koliko vas igra?") 
-     (get-input)
-    (println "nas predlog je da se podelite u _ ekipa. Zelite li drugacije? 
-              1-da
-              2-ne")
-    (get-input)
-    (println "Sada nekoliko podataka o sobi i predlozicemo vam tim.")
-    (collect-data-to-determine)
-    (println "Ovo je ono sto imamo od podataka o igracima: djisgjodg
-              da li postoji nesto vazno sto treba naznaciti u vezi nekog
-              od igraca, a da nije pomenuto?
-              1-ne
-              2-unos")
-  (if (= "2" (get-input)) 
-    ( do(println "unesite:") (get-input))
-    (println "Za nekoliko sekundi videcete nas predlog timova!"))
-)
+(if (= "1" (get-input))
+  (println (inc score))
+  (println (inc score) (inc score))) (println score)))
+ 
+
+(defn podaci []
+  (println "koliko vas igra?")
+  (get-input)
+  (println "nas predlog je da se podelite u _ ekipa. Zelite li drugacije? 
+                1-da
+                2-ne")
+  (get-input)
+  (println "Sada nekoliko podataka o sobi i predlozicemo vam tim.")
+  (collect-data-to-determine)
+  (println "Ovo je ono sto imamo od podataka o igracima: djisgjodg
+                da li postoji nesto vazno sto treba naznaciti u vezi nekog
+                od igraca, a da nije pomenuto?
+                1-ne
+                2-unos")
+  (if (= "2" (get-input))
+    (do (println "unesite:") (get-input))
+    (println "Za nekoliko sekundi videcete nas predlog timova!")))
+
+(defn prompt [question]
+  (println question)
+  (flush)
+  (read-line))
+
+
+
+
+
+(defn get-user-info []
+  {:name (prompt "Tell me about yourself. Name?")
+   :experience (Integer. (prompt "How experienced from 1 (not at all) to 5 (expert) are you with escape rooms?"))
+   :adroit (Integer. (prompt "How adroit from 1 (not at all) to 5 (extremely) you find yourself?"))
+   :mood (Integer. (prompt "How would you describe your mood today from 1 (bad) to 5 (cheerfull and excited) you find yourself?"))
+   :teamplayer ( Integer. (prompt "Do you find yourself a team player? 1-yes, 2-no"))
+   :frightened (Integer. (prompt "Are you scared of the jumpscares and creepy details? 1-yes, 2-no"))
+   :theme (Integer. (prompt "Does the theme of the room interest you or is related to you anyhow that can be an advangate? 1-yes, 2-no"))
+   :competitiveness (Integer. (prompt "Do you find yourself competitive? 1-yes, 2-no")) 
+   }
+  )
+
+(def ^:dynamic *players* [])
+
+
+(defn players-vector-maker []
+   (let [num-users (Integer. (prompt "How many users?"))
+        user-info (doall (repeatedly num-users get-user-info))]
+    (alter-var-root #'*players*
+                    (fn [current-players]
+                      (concat current-players user-info)))
+    (println "OK! Thank you.")
+    (println "User information stored in memory:")
+    (prn *players*)))
+
+(defn -main [& args]
+ (players-vector-maker))
     
   
   
