9498f14bf1c2c9b77020663366f8f0f515dc7d16
martodmih <martodmih@gmail.com>
2024-01-04 20:39:42 +0100

    uredjen kod u logic.clj

--- escape-game/src/escape_game/logic.clj
+++ escape-game/src/escape_game/logic.clj
@@ -1,25 +1,25 @@
 (ns logic
   (:require [clojure.string]
-            [incanter.core]
-            ))
+            [incanter.core]))
+
 (defn my-into
   [target additions]
   (apply conj additions target))
 
-(def igraci [{:name "Nina" :experience 1 :teamplayer 1 :adroit 3 :mood 5 :theme 2 :frightened 2 :competitiveness 2 }
-             {:name "Anis" :experience  4 :teamplayer 1 :adroit 4 :mood 2 :theme 1 :frightened  1 :competitiveness 1 }
-             {:name "Điri" :experience  5 :teamplayer 2 :adroit 1 :mood 5 :theme  2 :frightened 2 :competitiveness 1 }
-             {:name "Joča" :experience  2 :teamplayer 1 :adroit 3 :mood 1 :theme  1 :frightened  1 :competitiveness 2 }
-             {:name "Maša" :experience  5 :teamplayer 2 :adroit 4 :mood 2 :theme  2 :frightened  2 :competitiveness 1 }
-            {:name "Nemica" :experience 1 :teamplayer 1 :adroit 3 :mood 2 :theme  2 :frightened  1 :competitiveness 2 }
-             {:name "Roko" :experience 5 :teamplayer 2 :adroit 1 :mood 3 :theme  2 :frightened  2 :competitiveness 1 }
-             {:name "Roki" :experience  4 :teamplayer 1 :adroit 4 :mood 1 :theme  2 :frightened  1 :competitiveness 2 }
-             {:name"Teo" :experience  1 :teamplayer 1 :adroit 2 :mood 5 :theme  2 :frightened  1 :competitiveness 2 }
-            {:name "Nelsi" :experience  3 :teamplayer 2 :adroit 3 :mood 4 :theme  1 :frightened  1 :competitiveness 2  }
-            {:name "Beli" :experience  5 :teamplayer 2 :adroit 1 :mood 2 :theme  1 :frightened  2 :competitiveness 1  }
-             {:name "Cole" :experience  4 :teamplayer 1 :adroit 5 :mood 1 :theme  1 :frightened 1 :competitiveness 1  }
-             {:name "Bebinger" :experience  1 :teamplayer 1 :adroit 4 :mood 5 :theme  2 :frightened  2 :competitiveness 1 }
-             ])
+(def igraci
+  [{:name "Nina" :experience 1 :teamplayer 1 :adroit 3 :mood 5 :theme 2 :frightened 2 :competitiveness 2}
+   {:name "Anis" :experience 4 :teamplayer 1 :adroit 4 :mood 2 :theme 1 :frightened 1 :competitiveness 1}
+   {:name "Điri" :experience 5 :teamplayer 2 :adroit 1 :mood 5 :theme 2 :frightened 2 :competitiveness 1}
+   {:name "Joča" :experience 2 :teamplayer 1 :adroit 3 :mood 1 :theme 1 :frightened 1 :competitiveness 2}
+   {:name "Maša" :experience 5 :teamplayer 2 :adroit 4 :mood 2 :theme 2 :frightened 2 :competitiveness 1}
+   {:name "Nemica" :experience 1 :teamplayer 1 :adroit 3 :mood 2 :theme 2 :frightened 1 :competitiveness 2}
+   {:name "Roko" :experience 5 :teamplayer 2 :adroit 1 :mood 3 :theme 2 :frightened 2 :competitiveness 1}
+   {:name "Roki" :experience 4 :teamplayer 1 :adroit 4 :mood 1 :theme 2 :frightened 1 :competitiveness 2}
+   {:name "Teo" :experience 1 :teamplayer 1 :adroit 2 :mood 5 :theme 2 :frightened 1 :competitiveness 2}
+   {:name "Nelsi" :experience 3 :teamplayer 2 :adroit 3 :mood 4 :theme 1 :frightened 1 :competitiveness 2}
+   {:name "Beli" :experience 5 :teamplayer 2 :adroit 1 :mood 2 :theme 1 :frightened 2 :competitiveness 1}
+   {:name "Cole" :experience 4 :teamplayer 1 :adroit 5 :mood 1 :theme 1 :frightened 1 :competitiveness 1}
+   {:name "Bebinger" :experience 1 :teamplayer 1 :adroit 4 :mood 5 :theme 2 :frightened 2 :competitiveness 1}])
 
 (defn print-teams [balanced-teams]
   (let [format-team (fn [team]
@@ -33,60 +33,46 @@
               new-result (str result "Team " (inc idx) ": " team-str "\n")]
           (recur (inc idx) (rest teams) new-result))))))
 
-(defn check-topic
-  [igrac]
+(defn check-topic [igrac]
   (if (get igrac :theme)
     2
-    0)
- )
+    0))
 
-
-(defn check-fear
-  [igrac]
-  (if (= 1 (get igrac :frightened)) 
-   -1
+(defn check-fear [igrac]
+  (if (= 1 (get igrac :frightened))
+    -1
     2))
 
-(defn sum-good-attributes
-  [igrac]
-(+ (get igrac :mood) (get igrac :experience) (get igrac :adroit)))
+(defn sum-good-attributes [igrac]
+  (+ (get igrac :mood) (get igrac :experience) (get igrac :adroit)))
 
-
-(defn check-competitive
-  [igrac]
+(defn check-competitive [igrac]
   (if (= (get igrac :competitiveness) 1)
     -1
     1))
 
-(defn check-team-player
-  [igrac]
+(defn check-team-player [igrac]
   (if (= (get igrac :teamplayer) 1)
     2
     0))
 
-(defn sum-everything
-  [igrac]
+(defn sum-everything [igrac]
   (assoc igrac :summary (+ (sum-good-attributes igrac) (check-fear igrac) (check-topic igrac) (check-competitive igrac) (check-team-player igrac))))
 
-
-(defn sort-everything
- [igraci]
+(defn sort-everything [igraci]
   (loop [i 0 sortiran []]
     (if (= (count igraci) i)
       sortiran
-   (recur (inc i) (sort-by :summary (my-into [(sum-everything (nth igraci i))] sortiran))))))
-
-
-
+      (recur (inc i) (sort-by :summary (my-into [(sum-everything (nth igraci i))] sortiran))))))
  
-(defn distribute-players-across-teams [players room-data num-teams ] 
- (sort-everything players)
+(defn distribute-players-across-teams [players room-data num-teams]
+  (sort-everything players)
   (let [total-players (count players)]
     (loop [front 0
            back (dec total-players)
            teams (vec (repeat num-teams []))]
       (cond
-        (> front back) teams 
+        (> front back) teams
         (= front back)
         (let [random-team (rand-int num-teams)]
           (update teams random-team conj (nth players front)))
@@ -103,9 +89,6 @@
                                 (str "Team " (inc idx) ": " (clojure.string/join ", " (map :name team))))
                               few-teams)]
       (println team))))
-
-
-
 
 ;sada cu da pokusam da napravim algoritam za podelu u tri ekipe. 
 ;ideja mi je da u odnosu na ovaj za podelu na dva unapredim algoritam tako sto ce se 
@@ -124,14 +107,10 @@
         linear-adjust (if (and (= 2 (:linear room-data)) (= 1 (:teamplayer player))) 1 0)]
     (+ base-coef horror-adjust knowledge-adjust tech-adjust linear-adjust)))
 
-
-
-
 (defn add-coefs-to-players [players room-data]
   (map (fn [player]
          (assoc player :coef (calculate-coef player room-data)))
        players))
-
 
 (defn divide-players [players num-teams]
   (let [ensure-coef (fn [player] (if (nil? (:coef player)) (assoc player :coef 0) player))
@@ -145,13 +124,6 @@
                 updated-teams))
             initial-teams
             sorted-players)))
-
-
-
-
-
-
-
 
 ;ovo do sada je izostavilo uticaj toga da ti je igrac timski igrac ili kompetitivan
 ;jer ti faktori imaju znacaja tek nakon sto je inicijalna podela vec napravljena, zato sto sami od sebe
@@ -167,19 +139,12 @@
              (update player :coef + teamplayer-adjust competitive-adjust)))
          team)))
 
-
-
 (defn calculate-threshold [room-data teams]
   (let [base-threshold 8
-        team-size-factor (if (> (count (first teams)) 5) 1 0.8) ;; smaller threshold for smaller teams
-        linear-factor (if (= 1 (:linear room-data)) 1.2 1) ;; higher threshold for linear rooms
-        horror-factor (if (= 1 (:horror room-data)) 0.8 1)] ;; smaller threshold for horror rooms
+        team-size-factor (if (> (count (first teams)) 5) 1 0.8)
+        linear-factor (if (= 1 (:linear room-data)) 1.2 1)
+        horror-factor (if (= 1 (:horror room-data)) 0.8 1)]
     (int (* base-threshold team-size-factor linear-factor horror-factor))))
-
-
-
-
-
 
 (defn is-balanced [teams threshold]
   (let [team-coefs (map #(reduce + (map :coef %)) teams)
@@ -187,19 +152,11 @@
         min-coef (apply min team-coefs)]
     (<= (- max-coef min-coef) threshold)))
 
-
-
 (defn redivide-if-unbalanced [teams original-players threshold]
   (if (is-balanced teams threshold)
     teams
     (let [updated-players (apply concat (map adjust-coefs-for-traits teams))]
       (divide-players updated-players (count teams)))))
-
-
-
-
-
-
 
 ;sledecu funkciju pravim da bih lakse pozvala ceo ovaj algoritam iz drugog namespace-a
 (defn create-and-print-balanced-teams [players room-data num-teams]
@@ -212,37 +169,24 @@
                               balanced-teams)]
       (println team))))
 
-
-
-
-
-;za algoritam za podelu na 4 pokusacu da koristim k-means algoritam koristeci biblioteku Incanter.
-;iako su vrednosti atributa za igrace dobro rasporedjene (1-5) prvo cu ih normalizovati kako bi rezultati bili optimalniji
-
-
-
+;sada implementiram k-means algoritam
 (defn scale-value [x]
   (/ (- x 1) 4))
- 
 
-;nesto ocigledno nije u redu sa okruzenjem i ne mogu vise da gubim vreme
-;na ucitavanje biblioteke tako da cu implementirati neku pocetnicku verziju k-meansa bez biblioteke
-;prvo osnovne funkcije koje ce biti potrebne
 (defn euclidean-distance [vec1 vec2]
   (Math/sqrt (reduce + (map #(Math/pow (- %1 %2) 2) vec1 vec2))))
-
 
 (defn average [coll]
   (/ (reduce + coll) (count coll)))
 
-
-
 (defn mean [vectors]
   (map average (apply map vector vectors)))
-;uzimamo vrednosti atributa koje cemo koristiti za grupisanje
-(defn extract-features [player]
-  [(scale-value(player :experience) )(scale-value(player :adroit) )(scale-value(player :mood)) (scale-value(player :theme))])
 
+(defn extract-features [player]
+  [(scale-value (player :experience))
+   (scale-value (player :adroit))
+   (scale-value (player :mood))
+   (scale-value (player :theme))])
 
 ;funkcija map prolazi kroz igrace i onda svakog od njih dodeljuje centroidu kom je najblizi 
 ;(if to obavlja unutar funkcije reduce koja vektor sa mapama igraca
@@ -269,12 +213,12 @@
 (defn update-centroids [players assignments k]
   (let [grouped-players (group-by #(get assignments %) players)]
     (into {} (map (fn [i]
-                    [i (mean (vec (map extract-features (get grouped-players i))))]) 
+                    [i (mean (vec (map extract-features (get grouped-players i))))])
                   (range k)))))
 
 ;ovu funkciju sam delom prepisala sa gitHuba (deo gde se dodeljuju inicijalni centroidi), prilagodivsi je mojim kriterijumima i do sada napravljenim funkcijama
 (defn k-means-players [players k]
-  (let [initial-centroids (into {} (map-indexed (fn [i _] [i (extract-features (nth players i))]) (range k))) 
+  (let [initial-centroids (into {} (map-indexed (fn [i _] [i (extract-features (nth players i))]) (range k)))
         max-iterations 50]
     (loop [centroids initial-centroids
            n 0]
@@ -285,54 +229,41 @@
           (recur new-centroids (inc n)))))))
 ;max-iterations sam postavila na 50 (nasumicno) da bih izbegla rizik od overflow, testiranjem cu kasnije utvrditi da li treba da bude neki drugi broj
 
-
-
 ;ova fukcija treba da podeli igrace u timove na osnovu rezultata k-meansa.Prvo ce svakom timu dodeliti po jednog igraca iz svakog klastera
 ;(nastavice sa takvom podelom sve dok ima dovoljno igraca u svim klasterima), a onda ce preostale igrace podeliti ravnomervno tako da timovi imaju
 ;isto ili priblizno isti broj igraca. 
 (defn divide-players-into-teams [assignments players num-teams]
-  (let [cluster-groups (group-by #(get assignments %) players)] 
-    (let [team-assignments (reduce
-                            (fn [teams cluster-group]
-                              (reduce
-                               (fn [teams player]
-                                 (let [team-index (->> teams
-                                                       (map-indexed (fn [idx team] [idx (count team)]))
-                                                       (sort-by second)
-                                                       (first)
-                                                       (first))] 
-                                   (update teams team-index conj (:name player))))
-                               teams
-                               cluster-group))
-                            (vec (repeat num-teams []))
-                            (vals cluster-groups))] 
+  (let [cluster-groups (group-by #(get assignments %) players)]
+    (let [team-assignments (reduce (fn [teams cluster-group]
+                                     (reduce (fn [teams player]
+                                               (let [team-index (->> teams
+                                                                     (map-indexed (fn [idx team] [idx (count team)]))
+                                                                     (sort-by second)
+                                                                     (first)
+                                                                     (first))]
+                                                 (update teams team-index conj (:name player))))
+                                             teams
+                                             (second cluster-group)))
+                                   (vec (repeat num-teams []))
+                                   (vals cluster-groups))]
       team-assignments)))
-
-
 ;pravim funkcijju koja objedinjuje sve ovo da bih je lakse pozvala iz drugog namespace-a
 (defn create-and-print-k-means-teams [players room-data num-teams]
   (let [k-means-result (k-means-players players num-teams)
         assignments (:assignments k-means-result)
         teams (divide-players-into-teams assignments players num-teams)
         formatted-teams (map-indexed (fn [idx team]
-                                       (str "Team " (inc idx) ": " (clojure.string/join ", " team))) teams)]
-    (do (doseq [team formatted-teams]
-          (println team)) 
-       )))
+                                       (str "Team " (inc idx) ": " (clojure.string/join ", " team)))
+                                     teams)]
+    (doseq [team formatted-teams]
+      (println team))))
  
-
-
-
-
-
-
-
 ;sledeci algoritam delice igrace na osnovu vrednosti njihovih atributa, ali ce njima prethodno biti
 ;dodati tezinski koeficijenti koji zavise i od tipa sobe koji se igra, prvo definisemo inicijalne koeficijente
 ;razlika u odnosu na vec prethodno implementirani algoritam je sto je taj radio sa ukupnim koeficijenotm,
 ;a sada varira svaka od vrednosti pojedinacno
 
-(defn adjust-weights-based-on-room [room-data] 
+(defn adjust-weights-based-on-room [room-data]
   (let [base-weights {:experience 0.4, :adroit 0.35, :mood 0.3, :teamplayer 0.25, :competitiveness 0.2, :theme 0.15, :frightened 0.1}]
     (-> base-weights
         (update :adroit #(if (= (:tech room-data) 1) (* % 1.3) %))
@@ -340,12 +271,10 @@
         (update :frightened #(if (= (:horror room-data) 1) (* % 1.6) %))
         (update :theme #(if (= (:knowledge room-data) 1) (* % 1.7) %))
         (update :experience #(if (= (:linear room-data) 1) (* % 1.4) %)))))
-
 ;za igraca racunamo skor na osnovu njihovih atributa i odgovarajucih tezinskih koeficijenata
 (defn calculate-player-score [player weights]
   (reduce + (map (fn [[attr weight]] (* (get player attr 0) weight)) weights)))
 
-;podela igraca u timove na osnovu skora tako da svaki tim bude priblizno jednake snage
 (defn divide-players-into-teams-by-score [players num-teams adjusted-weights]
   (let [player-scores (map #(calculate-player-score % adjusted-weights) players)
         sorted-players (map first (sort-by second > (zipmap players player-scores)))
@@ -360,9 +289,6 @@
             initial-teams
             sorted-players)))
 
-
-
-
 (defn create-and-print-divisions-by-score [players room-data num-teams]
   (let [adjusted-weights (adjust-weights-based-on-room room-data)
         team-assignments (divide-players-into-teams-by-score players num-teams adjusted-weights)]
@@ -370,9 +296,6 @@
                                 (str "Team " (inc idx) ": " (clojure.string/join ", " (map :name team))))
                               team-assignments)]
       (println team))))
-
-
-
 
 ;algoritam koji cu sada implementirati je prilagodjeni Round Robin algoritam s tim sto cu na 
 ;specifican nacin soritrati igrace. Umesto da se rangiranje vrsi na osnovu zbira vrednosti njihovih atributa
@@ -395,7 +318,6 @@
    (double (:frightened player))
    (double (:competitiveness player))])
 
-
 (defn round-robin-distribute [players room-data num-teams]
   (let [player-std-devs (map (fn [player] [player (standard-deviation (player-attributes-list player))]) players)
         sorted-players (map first (sort-by second player-std-devs))
@@ -409,6 +331,7 @@
                 (update teams team-index conj player)))
             initial-teams
             sorted-players)))
+
 (defn create-and-print-round-robin-teams [players room-data num-teams]
   (let [round-robin-teams (round-robin-distribute players room-data num-teams)]
     (doseq [team (map-indexed (fn [idx team]
@@ -416,7 +339,6 @@
                               round-robin-teams)]
       (println team))))
 
-         
 
 ;ubacicu i algoritam za nasumicnu raspodelu kako bih kasnije poredila rezultate
 ;jedino o cemu algoritam vodi racuna je da bude jednak ili priblizno jednak broj igraca u svim timovima (obezbedjeno mod funkcijom)
@@ -432,6 +354,7 @@
                 (update teams team-index conj player)))
             initial-teams
             shuffled-players)))
+
 (defn create-and-print-randomly-made-teams [players room-data num-teams]
   (let [randomly-teams (distribute-players-randomly players room-data num-teams)]
     (doseq [team (map-indexed (fn [idx team]
@@ -439,13 +362,9 @@
                               randomly-teams)]
       (println team))))
 
-  
-  
+(def room-for-test
+  {:horror 2 :linear 1 :tech 1 :knowledge 2})
 
- 
-
- (def room-for-test
-   {:horror 2 :linear 1 :tech 1 :knowledge 2})
 
 
 
